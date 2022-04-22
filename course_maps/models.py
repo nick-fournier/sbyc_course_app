1e803a155b1cadf3bc0ee82b7ea1514d6f79037c
nick-fournier <nichfournier@gmail.com>
2022-04-21 21:48:42 -0700

    setup database

--- course_maps/models.py
+++ course_maps/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Marks(models.Model):
+    name = models.CharField(max_length=12, unique=True, primary_key=True)
+    coord_string = models.CharField(max_length=24, null=True)
+    format = models.CharField(max_length=24, null=True)
+    precision_value = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    precision_units = models.CharField(max_length=12)
+
+class CourseObjects(models.Model):
+    name = models.CharField(max_length=12, unique=True, primary_key=True)
+    description = models.CharField(max_length=255)
+    type = models.CharField(max_length=12)
+    points = models.ForeignKey(Marks, on_delete=models.CASCADE, null=True)
+
+class CourseMetaData(models.Model):
+    course_number = models.IntegerField(primary_key=True)
+    # Calculate this on initialization
+    #distance = models.DecimalField(max_digits=12, decimal_places=4, default=None, null=True)
+
+class Courses(models.Model):
+    course = models.ForeignKey(CourseMetaData, on_delete=models.CASCADE)
+    mark = models.ForeignKey(Marks, on_delete=models.CASCADE)
+    rounding_direction = models.CharField(max_length=1)
+    mark_order = models.IntegerField(primary_key=True)
+
+
+

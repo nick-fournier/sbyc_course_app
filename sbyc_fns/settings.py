8c2819afc39b114a6852210d7330ee6baa498f59
nick-fournier <nichfournier@gmail.com>
2022-04-21 19:02:23 -0700

    hid secrete key

--- sbyc_fns/settings.py
+++ sbyc_fns/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(5=$^)rt^jpo#jhnf5i0bs&%$hij_sac9&1n)4f4g#&0ye(^nd'
+SECRET_KEY = os.getenv('GCP_PROJECT_ID')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

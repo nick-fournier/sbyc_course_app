1ef5d7376d635704afb1c953e61cc69e0174c12f
nick-fournier <nichfournier@gmail.com>
2022-04-21 20:48:45 -0700

    shortened url to /maps/

--- sbyc_fns/urls.py
+++ sbyc_fns/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('course_maps/', include('course_maps.urls')),
+    path('maps/', include('course_maps.urls')),
     path('admin/', admin.site.urls),
 ]

b3071bcefe05c5d77939e3a0366819b1a086c5f6
nick-fournier <nichfournier@gmail.com>
2022-04-21 20:45:56 -0700

    basic webserver working

--- sbyc_fns/urls.py
+++ sbyc_fns/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('course_maps/', include('course_maps.urls')),
     path('admin/', admin.site.urls),
 ]

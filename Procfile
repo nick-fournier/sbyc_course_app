be502c56b905f98ba040080742e183197f77e557
nick-fournier <nichfournier@gmail.com>
2022-04-28 22:55:25 -0700

    added whtienoise and procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sbyc-fns.wsgi
--- sbyc_fns/settings.py
+++ sbyc_fns/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sbyc_fns.urls'
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

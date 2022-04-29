cb6e2a5945a612fffef1fa5fb16c6b8cd72038e7
nick-fournier <nichfournier@gmail.com>
2022-04-28 23:05:12 -0700

    added heroku to allowed hosts

--- sbyc_fns/settings.py
+++ sbyc_fns/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sbyc-fns.herokuapp.com']
 
 
 # Application definition

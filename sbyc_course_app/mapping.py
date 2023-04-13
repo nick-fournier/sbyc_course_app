0aec3a27943dd8a3de112d8bd98eff45c6c40ee6
nick-fournier <nichfournier@gmail.com>
2023-04-12 17:17:52 -0700

    significant speed improvement by loading data once

--- sbyc_course_app/mapping.py
+++ sbyc_course_app/mapping.py
@@ -11,6 +11,14 @@ from geographiclib.geodesic import Geodesic
 
 THIS_PATH = os.path.dirname(os.path.abspath(__file__))
 
+MAP_SETTINGS = {
+    'data_dir': os.path.join(THIS_PATH, 'fixtures/map_data'),
+    'course_data': {
+        'marks': 'course_marks.csv',
+        'objects': 'course_objects.yaml',
+        'order': 'course_order.yaml'}
+    }
+
 def coord_to_latlon(string):
     if not isinstance(string, str):
         lat, lon = [np.nan] * 2
@@ -39,7 +47,7 @@ def coord_diff(coord_df, units=hs.Unit.METERS):
 
 def get_bearing(point_list):
     lat1, long1, lat2, long2 = [x for point in point_list for x in point]
-    bearing = Geodesic.WGS84.Inverse(lat1, long1, lat2, long2)['azi1']
+    bearing = getattr(Geodesic, 'WGS84').Inverse(lat1, long1, lat2, long2)['azi1']
     compass_bearing = (bearing + 360) % 360
     return compass_bearing
 
@@ -54,36 +62,67 @@ def get_bearing_man(point_list):
     compass_bearing = (bearing + 360) % 360
     return compass_bearing
 
+
+
+# load_static_data once for session
+MAP_DATA = {}
+for key, file in MAP_SETTINGS['course_data'].items():
+    fpath = os.path.join(MAP_SETTINGS['data_dir'], file)
+    data = None
+    
+    if '.csv' in file:
+        data = pd.read_csv(fpath)
+
+    with open(fpath) as f:
+        if '.yaml' in file:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        if '.json' in file:
+            data = json.load(f)
+    
+    # Data formatting
+    if key == 'marks':
+        assert isinstance(data, pd.DataFrame)
+        data[['lat', 'lon']] = data.coord_string.apply(coord_to_latlon)
+        data.set_index('name', inplace=True)
+        # data.index = data.name
+
+    if key == 'objects':
+        assert isinstance(data, list)
+        data = {x.pop('name'): x for x in data}
+
+    if key == 'order':
+        assert isinstance(data, list)
+        data = {str(x.pop('course_number')): pd.DataFrame(x['marks']) for x in data}
+        for k in data.keys():
+            data[k].set_index('name', inplace=True)
+
+    MAP_DATA[key] = data
+
+
 class CourseCharting:
-    def __init__(self, **kwargs):
-        # Set default kwargs
-        defaultKwargs = {'static': True,
-                         'pin': 'T1',
-                         'rounding': 'PORT',
-                         'custom_coords': {},
-                         'is_mobile': False}
-        kwargs = {**defaultKwargs, **kwargs}
-
-        # Setup class vars
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-        self.marks = self.objects = self.order = None
-        # self.course_number = course_number
-        self.settings = {'data_dir': os.path.join(THIS_PATH, 'fixtures/map_data'),
-                         'course_data': {
-                             'marks': 'course_marks.csv',
-                             'objects': 'course_objects.yaml',
-                             'order': 'course_order.yaml'}
-                         }
-        if self.static:
-            self.load_static_data()
+    def __init__(self, **kwargs):               
+        # default kwargs, explicit types
+        self.course_number = kwargs.get('course_number')
+        self.is_mobile = kwargs.get('is_mobile', False)
+        self.custom_coords = kwargs.get('custom_coords', {})
+        self.rounding = kwargs.get('rounding', 'PORT')
+        self.pin = kwargs.get('pin', 'T1')
+        
+        # Defaults from map data
+        self.order = MAP_DATA.get('order')
+        self.objects = MAP_DATA.get('objects')
+        self.marks = MAP_DATA.get('marks')
+            
 
     def plot_course(self):
 
         #TODO map doesn't scale, need to get pixels somehow...
-        if self.is_mobile:
+        if getattr(self, 'is_mobile'):
             height = '100%'
+
+        assert self.marks is not None
+        assert self.order is not None
+        assert self.objects is not None
 
         # Map overall center point
         center = coord_mean(self.marks[['lat', 'lon']])
@@ -91,13 +130,13 @@ class CourseCharting:
         m = folium.Map(location=center, zoom_start=13, height='100%', width='100%')
 
         segment_points = []
-        course_df = copy.deepcopy(self.order[str(self.course_number)])
+        course_df = self.order[str(self.course_number)].copy()
 
-        for index, course_object in self.order[str(self.course_number)].iterrows():
+        for mark_name, course_object in course_df.iterrows():
 
             # if len(self.objects[course_object.name]['points']) > 1:
             if course_object.rounding is not None and course_object.rounding.upper() == 'GATE':
-                object_marks = self.objects[course_object.name]['points']
+                object_marks = self.objects[mark_name]['points']
                 object_coords = self.marks.loc[object_marks]
 
                 # If START, select which pin
@@ -141,10 +180,10 @@ class CourseCharting:
                     mark = copy.deepcopy(self.marks.loc[course_object.name])
 
             # Add the rounding to the chart table
-            if course_object.name in ['W', 'R']:
-                course_df.loc[mark.name, 'rounding'] = self.rounding
+            if mark_name in ['W', 'R']:
+                course_df.loc[mark_name, 'rounding'] = self.rounding
             else:
-                course_df.loc[mark.name, 'rounding'] = course_object.rounding
+                course_df.loc[mark_name, 'rounding'] = course_object.rounding
 
             mark_coords = mark[['lat', 'lon']].to_list()
             folium.Circle(
@@ -171,39 +210,45 @@ class CourseCharting:
         # Update map center
         m.location = coord_mean(pd.DataFrame(segment_points, columns=['lat', 'lon']))
         course_df.bearing = course_df.bearing.round().astype('Int64')
-        course_df = course_df[['order', 'name', 'rounding', 'bearing', 'lat', 'lon']]
+        course_df = course_df[['order', 'rounding', 'bearing', 'lat', 'lon']]
+        course_df.reset_index()
         course_df.columns = [s.capitalize() for s in course_df.columns]
 
         # m.save('templates/test_map.html')
         return {'chart': m, 'chart_table': course_df, 'course_number': self.course_number}
 
-    def load_static_data(self):
-        for key, file in self.settings['course_data'].items():
-            fpath = os.path.join(self.settings['data_dir'], file)
+    # def load_static_data(self):
+    #     for key, file in self.settings['course_data'].items():
+    #         fpath = os.path.join(self.settings['data_dir'], file)
+    #         data = None
+            
+    #         if '.csv' in file:
+    #             data = pd.read_csv(fpath)
 
-            if '.csv' in file:
-                data = pd.read_csv(fpath)
+    #         with open(fpath) as f:
+    #             if '.yaml' in file:
+    #                 data = yaml.load(f, Loader=yaml.FullLoader)
+    #             if '.json' in file:
+    #                 data = json.load(f)
+            
+    #         # Data formatting
+    #         if key == 'marks':
+    #             assert isinstance(data, pd.DataFrame)
+    #             data[['lat', 'lon']] = data.coord_string.apply(coord_to_latlon)
+    #             data.set_index('name', inplace=True)
+    #             # data.index = data.name
 
-            with open(fpath) as f:
-                if '.yaml' in file:
-                    data = yaml.load(f, Loader=yaml.FullLoader)
-                if '.json' in file:
-                    data = json.load(f)
+    #         if key == 'objects':
+    #             assert isinstance(data, list)
+    #             data = {x.pop('name'): x for x in data}
 
-            # Data formatting
-            if key == 'marks' and isinstance(data, pd.DataFrame):
-                data[['lat', 'lon']] = data.coord_string.apply(coord_to_latlon)
-                data.index = data['name']
+    #         if key == 'order':
+    #             assert isinstance(data, list)
+    #             data = {str(x.pop('course_number')): pd.DataFrame(x['marks']) for x in data}
+    #             for k in data.keys():
+    #                 data[k].set_index('name', inplace=True)
 
-            if key == 'objects':
-                data = {x.pop('name'): x for x in data}
-
-            if key == 'order':
-                data = {str(x.pop('course_number')): pd.DataFrame(x['marks']) for x in data}
-                for k, v in data.items():
-                    v.index = v['name']
-
-            setattr(self, key, data)
+    #         setattr(self, key, data)
 
 
 if __name__ == "__main__":

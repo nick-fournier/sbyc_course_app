937f26cef9a22edc0947fc1ed1f3416de1f6368c
nick-fournier <nichfournier@gmail.com>
2023-04-19 09:29:41 -0700

    update url tags

--- sbyc_course_app/urls.py
+++ sbyc_course_app/urls.py
@@ -5,5 +5,5 @@ from . import forms
 
 urlpatterns = [
     path('<int:course_number>-<str:pin>-<str:rounding>/', views.ChartView.as_view(), name='chart-view'),
-    path('', views.ChartView.as_view()),
+    path('', views.ChartView.as_view(), name='chart-view-index'),
 ]

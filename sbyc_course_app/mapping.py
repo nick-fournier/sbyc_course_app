9c3ad9df2d63c3da1484587d5ea5af463698d6a4
Nick Fournier <nichfournier@gmail.com>
2024-06-19 10:20:41 -0700

    cleaned up index and added prints

--- sbyc_course_app/mapping.py
+++ sbyc_course_app/mapping.py
@@ -2,11 +2,10 @@ import yaml
 from pathlib import Path
 from .utils import chart_course, coord_str_to_dec
 
-
-print('Loading map data...')
 # load_static_data once for session
 MAP_DATA = {}
 for obj in ['marks', 'objects', 'order']:
+    print(f'Loading {obj} data...')
     fpath = Path(__file__).parent / 'fixtures' / 'map_data' / f"course_{obj}.yaml"
 
     with open(fpath) as f:
@@ -17,6 +16,6 @@ for obj in ['marks', 'objects', 'order']:
             MAP_DATA[obj][key]['lat'] = coord_str_to_dec(MAP_DATA[obj][key]['lat'])
             MAP_DATA[obj][key]['lon'] = coord_str_to_dec(MAP_DATA[obj][key]['lon'])
 
-
 # Pre-compute course data
+print("Pre-computing course data...")
 COURSE_DATA = {i: chart_course(i, MAP_DATA) for i in MAP_DATA['order']}
--- sbyc_course_app/urls.py
+++ sbyc_course_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('gpx/<int:course_number>-<str:pin>-<str:rounding>', views.gpx_response_raw, name='gpx_raw'),
     path('chart-app/', views.chart_app, name='chart-app'),
     path('flags/', views.flag_table, name='flag-table'),
-    path('', views.index, name='index'),    
+    path('', views.index, name='sbyc_course_app-index'),    
 ]
